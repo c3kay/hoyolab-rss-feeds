0e7c4fcd972491f8fd3f83dcbeebadd66780a970
Calvin <c3kay@icloud.com>
2021-08-03 12:21:02 +0200

    minor improvements

--- hoyolab.py
+++ hoyolab.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import requests
 import json
 from os import environ
@@ -104,8 +102,11 @@ def create_feed_file(path, url, icon, items):
         'items': items
     }
 
-    with open(path, 'w') as fd:
-        json.dump(feed, fd)
+    try:
+        with open(path, 'w') as fd:
+            json.dump(feed, fd)
+    except IOError:
+        print('[create_feed_file] Could not write json file to {}!'.format(path))
 
 
 def load_feed_items(path, num_entries):
@@ -123,10 +124,9 @@ def collect_known_ids(feed_items):
     known_ids = {}
 
     for item in feed_items:
+        modified_ts = 0
         if 'date_modified' in item:
             modified_ts = datetime.fromisoformat(item['date_modified']).timestamp()
-        else:
-            modified_ts = 0
 
         known_ids[item['id']] = modified_ts
 
@@ -151,26 +151,35 @@ def main():
     # create list of all news items from all 3 categories
     all_posts = []
     for cat in range(1, 4):
-        all_posts.extend(request_news(cat, num_entries, mhyuuid))
+        cat_news = request_news(cat, num_entries, mhyuuid)
+
+        if cat_news is None:
+            return
+
+        all_posts.extend(cat_news)
+
 
     fetched_ids = {p['post']['post_id']: p['last_modify_time'] for p in all_posts}
 
-    # get posts which need to be fetched
+    # get posts which need to be fetched or updated
     new_ids = []
     for post_id, modified in fetched_ids.items():
         if post_id not in known_ids or (post_id in known_ids and modified > known_ids[post_id]):
             new_ids.append(post_id)
 
-    # remove modified items and create as deque
+    # remove modified items and create feed as deque
     if len(new_ids) > 0:
         feed_items = deque(filter(lambda x: x['id'] not in new_ids, feed_items))
 
     # prepend items to existing feed
     for post_id in new_ids:
         post = request_post(post_id, mhyuuid)
-        if post is not None:
-            new_item = create_feed_item(post)
-            feed_items.appendleft(new_item)
+
+        if post is None:
+            return
+
+        new_item = create_feed_item(post)
+        feed_items.appendleft(new_item)
 
     create_feed_file(feed_path, feed_url, feed_icon, list(feed_items))
 
